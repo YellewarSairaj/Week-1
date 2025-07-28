2a2cab81f6f2092dbcb4bbd2f6f73e393e18eb34
YellewarSairaj <2420030450@klh.edu.in>
2025-07-28 11:39:02 +0530

    Alphabet.java

--- Week_1/src/Alphabet.java
+++ Week_1/src/Alphabet.java
@@ -1,4 +1,11 @@
-
+import java.util.Scanner;
 public class Alphabet {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println("Alphabet");
+        else
+            System.out.println("Not Alphabet");
+    }
 }
