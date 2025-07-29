16442d0fa3506746857374ec85fd7e1adc5d77b7
YellewarSairaj <2420030450@klh.edu.in>
2025-07-29 14:43:29 +0530

    Add files via upload

---
+++ Skill-1/src/TypeCast.java
@@ -0,0 +1,14 @@
+
+public class TypeCast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=123;
+      long b=a;
+      float c=b;
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+	}
+
+}
---
+++ Skill-1/src/WrapperClass.java
@@ -0,0 +1,12 @@
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=20;
+      Integer b=Integer.valueOf(a);
+      Integer c=a;
+      System.out.println(a+" "+b+" "+c);
+	}
+
+}
